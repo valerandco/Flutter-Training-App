d57c954987d82d7f3f34fb7017ab02b38c18a088
rk6_student <2afeu33f>
2022-10-30 19:28:20 +0300

    add page video_info

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -288,91 +288,101 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
             
-            Expanded(child: ListView.builder(
-              itemCount: (info.length.toDouble()/2).toInt(),
-              itemBuilder: (_, i){
-
-                return Row(
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 170,
-                      padding: EdgeInsets.only(bottom: 5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        image: DecorationImage(
-                          image: AssetImage(
-                            info[i]["img"],
+            Expanded(child: OverflowBox(
+              maxWidth: MediaQuery.of(context).size.width,
+              child: MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: ListView.builder(
+                  itemCount: (info.length.toDouble()/2).toInt(),
+                  itemBuilder: (_, i){
+                    int a = 2*i; //0
+                    int b = 2*i + 1; // 1
+                    return Row(
+                      children: [
+                        Container(
+                          width: (MediaQuery.of(context).size.width-90)/2,
+                          height: 170,
+                          margin: EdgeInsets.only(left: 30, bottom: 15, top: 15 ),
+                          padding: EdgeInsets.only(bottom: 5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                              image: AssetImage(
+                                info[a]["img"],
+                              ),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 3,
+                                offset: Offset(5, 5),
+                                color: AppColor.gradientSecond.withOpacity(0.1),
+                              ),
+                              BoxShadow(
+                                blurRadius: 3,
+                                offset: Offset(-5,-5),
+                                color: AppColor.gradientSecond.withOpacity(0.1),
+                              ),
+                            ],
                           ),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 3,
-                            offset: Offset(5, 5),
-                            color: AppColor.gradientSecond.withOpacity(0.1),
-                          ),
-                          BoxShadow(
-                            blurRadius: 3,
-                            offset: Offset(-5,-5),
-                            color: AppColor.gradientSecond.withOpacity(0.1),
-                          ),
-                        ],
-                      ),
-                      child: Center(
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Text(
-                            info[i]["title"],
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: AppColor.homePageDetail,
+                          child: Center(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                info[a]["title"],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: AppColor.homePageDetail,
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                    Container(
-                      width: 200,
-                      height: 170,
-                      padding: EdgeInsets.only(bottom: 5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                        image: DecorationImage(
-                          image: AssetImage(
-                            info[i+1]["img"],
+                        Container(
+                          width: (MediaQuery.of(context).size.width-90)/2,
+                          height: 170,
+                          margin: EdgeInsets.only(left: 30, bottom: 15, top: 15),
+                          padding: EdgeInsets.only(bottom: 5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                              image: AssetImage(
+                                info[b]["img"],
+                              ),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 3,
+                                offset: Offset(5, 5),
+                                color: AppColor.gradientSecond.withOpacity(0.1),
+                              ),
+                              BoxShadow(
+                                blurRadius: 3,
+                                offset: Offset(-5,  -5),
+                                color: AppColor.gradientSecond.withOpacity(0.1),
+                              ),
+                            ],
                           ),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 3,
-                            offset: Offset(5, 5),
-                            color: AppColor.gradientSecond.withOpacity(0.1),
-                          ),
-                          BoxShadow(
-                            blurRadius: 3,
-                            offset: Offset(-5,  -5),
-                            color: AppColor.gradientSecond.withOpacity(0.1),
-                          ),
-                        ],
-                      ),
-                      child: Center(
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Text(
-                            info[i+1]["title"],
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: AppColor.homePageDetail,
+                          child: Center(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                info[b]["title"],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: AppColor.homePageDetail,
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                  ],
-                );
-              },
+                      ],
+                    );
+                  },
+                ),
+              ),
             ))
           ],
         ),
